58bea5dcab5af4e3e493a1f83c05cfea0c5b71fa
pauljproche <paul.jp.roche@gmail.com>
2023-08-02 19:05:26 -0400

    Update display_BASIC.js

--- display_BASIC.js
+++ display_BASIC.js
@@ -17,20 +17,38 @@ async function run() {
     // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
-    /*
     // (REF b - See HOWTO.md):
-    // get the database and collection
+    // Get the database and collection
     const database = client.db("taskConnect");
     const collection = database.collection("taskCard");
 
     // !!! "query" queries all collections (with {})
-    // toArray() optional - It converts returned query (aka:cursor/pointer) into an array of documents 
+    // toArray() optional - It converts the returned query (aka cursor/pointer) into an array of documents 
     // and displays things cleaner.
     const query = await collection.find({}).toArray();
-    */
 
     console.log("Documents in the 'taskCard' collection:");
-    // console.log(query);
+    console.log(query);
+
+    // Prepare the query results for display in the HTML response
+    let queryResultsHtml = "<h3>Query Results:</h3><ul>";
+    query.forEach((result) => {
+      queryResultsHtml += `<li>Title: ${result.title}, Author: ${result.author}</li>`;
+    });
+    queryResultsHtml += "</ul>";
+
+    // Start the HTTP server and respond with the HTML page
+    const port = process.env.PORT || 3000;
+
+    http.createServer(function (req, res) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.write("<h2>Hello World</h2>");
+      res.write("Success! This app is deployed online");
+      res.write(queryResultsHtml); // Output the query results
+      res.end();
+    }).listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
   } catch (err) {
     console.log("Error:", err);
   } finally {
@@ -40,14 +58,3 @@ async function run() {
 }
 
 run().catch(console.dir);
-
-const port = process.env.PORT || 3000;
-
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write("<h2>Hello World yoyoyo3245</h2>");
-  res.write("Success! This app is deployed online");
-  res.end();
-}).listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
