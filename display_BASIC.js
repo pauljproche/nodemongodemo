f383612edd9c131fe610cb4ba99f01feb7ba0a76
pauljproche <paul.jp.roche@gmail.com>
2023-08-02 19:01:38 -0400

    Update display_BASIC.js

--- display_BASIC.js
+++ display_BASIC.js
@@ -1,32 +1,38 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://taskconnect2:V02gss7wWBeSd47M@cluster0.szozfpl.mongodb.net/?retryWrites=true&w=majority"; 
+const http = require('http');
+
+const uri = "mongodb+srv://taskconnect2:V02gss7wWBeSd47M@cluster0.szozfpl.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-}); 
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-/*
-    //(REF b - See HOWTO.md):
-    //get the database and collection
-    const database = client.db("taskConnect");
-    let collection = database.collection("taskCard");
 
-    //!!! "query" queries all collections (with {})
+    /*
+    // (REF b - See HOWTO.md):
+    // get the database and collection
+    const database = client.db("taskConnect");
+    const collection = database.collection("taskCard");
+
+    // !!! "query" queries all collections (with {})
     // toArray() optional - It converts returned query (aka:cursor/pointer) into an array of documents 
     // and displays things cleaner.
-    let query = await collection.find({}).toArray();
-*/
+    const query = await collection.find({}).toArray();
+    */
+
     console.log("Documents in the 'taskCard' collection:");
- //   console.log(query);
+    // console.log(query);
+  } catch (err) {
+    console.log("Error:", err);
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
@@ -34,3 +40,14 @@ async function run() {
 }
 
 run().catch(console.dir);
+
+const port = process.env.PORT || 3000;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write("<h2>Hello World</h2>");
+  res.write("Success! This app is deployed online");
+  res.end();
+}).listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
