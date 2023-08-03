5ba3acfbc3ed1135a745e2d941292f0828c984f5
pauljproche <paul.jp.roche@gmail.com>
2023-08-02 20:52:31 -0400

    Update display_BASIC.js

--- display_BASIC.js
+++ display_BASIC.js
@@ -3,38 +3,27 @@ const http = require('http');
 
 const uri = "mongodb+srv://taskconnect2:V02gss7wWBeSd47M@cluster0.szozfpl.mongodb.net/?retryWrites=true&w=majority";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
 async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
 
-    // (REF b - See HOWTO.md):
-    // Get the database and collection
+  try {
+    await client.connect();
     const database = client.db("taskConnect");
     const collection = database.collection("taskCard");
 
-    // !!! "query" queries all collections (with {})
-    // toArray() optional - It converts the returned query (aka cursor/pointer) into an array of documents 
-    // and displays things cleaner.
     const query = await collection.find({}).toArray();
-
-    console.log("Documents in the 'taskCard' collection:");
-    console.log(query);
 
     return query;
   } catch (err) {
-    console.log("Error:", err);
+    console.log("Error in MongoDB query:", err);
+    throw err;
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
@@ -44,20 +33,21 @@ const port = process.env.PORT || 3000;
 http.createServer(async function (req, res) {
   if (req.url === '/') {
     try {
-      
       const queryResult = await run();
+
       res.writeHead(200, { 'Content-Type': 'text/html' });
       res.write("<h2>Hello World</h2>");
       res.write("Success! This app is deployed online");
 
       // Displaying the query results in the HTML response
-      /*res.write("<h3>Query Results:</h3>");
-      res.write("<pre>" + JSON.stringify(queryResult, null, 2) + "</pre>");*/
+      res.write("<h3>Query Results:</h3>");
+      res.write("<pre>" + JSON.stringify(queryResult, null, 2) + "</pre>");
 
       res.end();
     } catch (err) {
+      console.log("Error in request:", err);
       res.writeHead(500, { 'Content-Type': 'text/html' });
-      res.write("An error occurred while fetching data from MongoDB.");
+      res.write("An error occurred while processing the request.");
       res.end();
     }
   }
